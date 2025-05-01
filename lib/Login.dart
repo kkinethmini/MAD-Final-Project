bbaee1fe847ef8f65905230264018a6718ce63ca
Mmmm Gamage <mmmmgamage@Mmmms-MacBook-Air.local>
2025-05-01 16:25:58 +0530

    up1

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,3 +1,5 @@
+import 'package:black_hole/Appbardf.dart';
+import 'package:black_hole/Drewer.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -7,44 +9,9 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "edex1",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 24,
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.green,
-          automaticallyImplyLeading: false, // Disables default leading icon
-          actions: [
-            Builder(
-              builder:
-                  (context) => IconButton(
-                    icon: const Icon(Icons.menu),
-                    onPressed: () => Scaffold.of(context).openDrawer(),
-                  ),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const [
-              UserAccountsDrawerHeader(
-                accountName: Text("accountName"),
-                accountEmail: Text("accountEmail"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(""),
-                ),
-                decoration: BoxDecoration(color: Colors.blue),
-              ),
-              ListTile(leading: Icon(Icons.home), title: Text('Home')),
-              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
-            ],
-          ),
-        ),
+        appBar: Appbardf(),
+        drawer: Drewer(),
+
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
