aa54bd89f259e2273b8d35cd6b41a4a34605c847
kkinethmini <135637567+kkinethmini@users.noreply.github.com>
2025-05-30 19:47:30 +0530

    Update Appbardf.dart

--- Appbardf.dart
+++ Appbardf.dart
@@ -2,3 +2,7 @@
 import 'package:flutter/material.dart';
 
  
+// Defining a stateless widget called Appbardf that implements PreferredSizeWidget
+class Appbardf extends StatelessWidget implements PreferredSizeWidget {
+
+ 
