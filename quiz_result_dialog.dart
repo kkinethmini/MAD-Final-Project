a7bcdd741e3f2ec5d0d3ecfa4895779c31f100fa
wmipkumara <140998011+wmipkumara@users.noreply.github.com>
2025-05-30 10:31:25 +0530

    Update quiz_result_dialog.dart

--- quiz_result_dialog.dart
+++ quiz_result_dialog.dart
@@ -1,2 +1,8 @@
 import 'package:flutter/material.dart';
 void showQuizResultDialog({
+ required BuildContext context,
+  required int score,
+  required int totalQuestions,
+  required VoidCallback onPass,
+  required VoidCallback onFail,
+})
